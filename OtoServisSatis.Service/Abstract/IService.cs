35d4f6a009da837c0af3874b1b6caf83d0719c69
Grkn3 <121625631+Grkn3@users.noreply.github.com>
2023-06-09 00:58:47 +0300

    09/06/2023

--- OtoServisSatis.Service/Abstract/IService.cs
+++ OtoServisSatis.Service/Abstract/IService.cs
@@ -1,10 +1,5 @@
 ﻿using OtoServisSatis.Data.Abstract;
 using OtoServisSatis.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OtoServisSatis.Service.Abstract
 {
